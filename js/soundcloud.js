3cd1ed807a7f556b91cc1b38a1917b96d116e303
aj07mm <aj07mm@hotmail.com>
2014-03-27 13:22:17 -0300

    lozl

---
+++ js/soundcloud.js
@@ -0,0 +1,16 @@
+SC.initialize({
+	client_id: "e517d415cdd1bf3053a497011cbf2339",
+}); 
+
+
+SC.stream("/rodrigobueno/loveee", function(tracks){
+	tracks.play();
+});
+
+SC.oEmbed("http://soundcloud.com/rodrigobueno", {
+	color: "ff0066",
+	maxwidth: '600px',
+	maxheight: '150px',
+	show_comments: 'true',
+	iframe: 'true'
+},document.getElementById("soundcloud-player"));
